b69e79e7b6dca903b5ca12ac37f8dbc9a11c1c2e
Jeffrey Luong <jjck1988@gmail.com>
2015-09-27 13:00:52 -0400

    added ngCordova dependecy to control ios status bar

--- www/js/app.js
+++ www/js/app.js
@@ -3,17 +3,21 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var app = angular.module('hueSquare', ['ionic', 'ngCordova']);
 
-.run(function($ionicPlatform) {
+app.run(function($ionicPlatform, $cordovaStatusbar) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
+    $cordovaStatusbar.hide();
+
     if(window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
     if(window.StatusBar) {
       StatusBar.styleDefault();
     }
+
+
   });
 })
