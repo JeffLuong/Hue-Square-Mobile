8e9a76b02803e480e43ee43f6ab0731c32826863
Jeffrey Luong <jjck1988@gmail.com>
2015-09-29 10:02:54 -0400

    adding board, render, tile services

---
+++ www/js/services/board.js
@@ -0,0 +1,61 @@
+// For board object
+angular.module('hueSquare')
+
+  .factory('tile', function(baseColors) {
+    'use strict';
+
+    var Board = function(size) {
+      this.size  = size;
+      this.board = this.makeBoard();
+
+      this.makeBoard = function() {
+        var emptyBoard = [];
+        for (var x = 0; x < this.size; x++) {
+          var row = emptyBoard[x] = [];
+          for (var y = 0; y < this.size; y++) {
+            row.push(null);
+          };
+        };
+        return emptyBoard;
+      };
+
+      // Dupe board for ai to play
+      this.dupeBoard = function() {
+        var duped = new Board(this.size);
+
+        for (var row = 0; row < duped.size; row++) {
+          for (var col = 0; col < duped.size; col++) {
+            var tile = this.board[row][col].dupeTile();
+            duped.addTile(duplicateTile);
+          };
+        };
+
+        return duped;
+      };
+
+      this.returnPosition = function(y, x) {
+        return {x: x, y: y};
+      };
+
+      this.addTile = function(tile) {
+        this.board[tile.x][tile.y] = tile;
+      };
+
+      // Check if within bounds...returns true/false
+      this.inBounds = function(position) {
+        return position.x >= 0 && position.y >= 0 && position.x < this.size && position.y < this.size;
+      };
+
+      this.serializeBoard = function(board) {
+        currState = {
+          size       : this.size,
+          savedBoard : board
+        };
+
+        return currState;
+      }
+
+    };
+
+    return (Board);
+  });
---
+++ www/js/services/tile.js
@@ -0,0 +1,40 @@
+// For tile object
+angular.module('hueSquare')
+
+  .factory('tile', function(baseColors) {
+    'use strict';
+
+    var Tile = function(position, color) {
+      this.x              = position.x;
+      this.y              = position.y;
+      this.color          = color;
+      this.lastPosition   = null;
+      this.aiLastPosition = {x: 0, y: 0};
+
+      this.startPosition  = function() {
+        return {x: this.x, y: this.y};
+      };
+
+      this.saveLastPosition = function(position, isAiPlayer) {
+        if (isAiPlayer) {
+          this.aiLastPosition = {x: position.x, y: position.y};
+        } else {
+          this.lastPosition = {x: position.x, y: position.y};
+        }
+      };
+
+      this.updatePosition = function(position) {
+        this.x = position.x;
+        this.y = position.y;
+      };
+
+      this.dupeTile = function () {
+        return new Tile({
+          x: this.x,
+          y: this.y
+        }, this.color);
+      };
+    };
+
+    return (Tile);
+  });
