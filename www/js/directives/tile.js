3f858be98ccc89bb457bcd03ab365a5b1b4df056
Jeffrey Luong <jjck1988@gmail.com>
2015-09-30 16:37:51 -0400

    added template for board rows in tile.js directive

--- www/js/directives/tile.js
+++ www/js/directives/tile.js
@@ -5,11 +5,36 @@ angular.module('hueSquare')
     console.log("------------------------");
     return {
       restrict: 'A',
-      template: "<h1>HELLO WORLD</h1>",
+      template:
+      "<div ng-repeat='row in rows' class='board-row'>" +
+        "<div ng-repeat='square in squares' class='board-sq'>" +
+          "<div ng-repeat='tile in tiles' class='tile'></div>" +
+        "</div>" +
+      "</div>",
       controller: function($rootScope, $scope, $element) {
-        var game = new GameData;
-        var currGame = game.getCurrGame();
+        var game      = new GameData,
+            currGame  = game.getCurrGame(),
+            numOfRows = currGame.board.savedBoard.length;
         console.log(currGame.board.savedBoard);
+        console.log(currGame.board.savedBoard.length);
+        console.log($element[0]);
+
+
+        // Tile functions
+        function initTiles() {
+          addColors();
+        }
+
+        function addColors() {
+          for (var i = 0; i < numOfRows; i++) {
+            console.log("adding row");
+          }
+
+        }
+
+        
       }
+
+
     }
   });
